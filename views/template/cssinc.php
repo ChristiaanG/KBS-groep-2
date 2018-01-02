a80d42b7de9467219b404831bf88b7d848814f8d
Christiaan Goslinga <christiaan.goslinga@rysst.nl>
2018-01-02 19:32:59 +0100

    added correct links to css, js and config files

--- views/template/cssinc.php
+++ views/template/cssinc.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <link href="../../css/bootstrap.min.css" rel="stylesheet">
-    <link href="../../css/metisMenu.min.css" rel="stylesheet">
-    <link href="../../css/startmin.css" rel="stylesheet">
-    <link href="../../css/font-awesome.min.css" rel="stylesheet" type="text/css">
-
-</html>
+<link href="<?= (isset($_SERVER['HTTPS']) ? "https://" : "http://") . $_SERVER["HTTP_HOST"] ?>/css/bootstrap.min.css" rel="stylesheet">
+<link href="<?= (isset($_SERVER['HTTPS']) ? "https://" : "http://") . $_SERVER["HTTP_HOST"] ?>/css/metisMenu.min.css" rel="stylesheet">
+<link href="<?= (isset($_SERVER['HTTPS']) ? "https://" : "http://") . $_SERVER["HTTP_HOST"] ?>/css/startmin.css" rel="stylesheet">
+<link href="<?= (isset($_SERVER['HTTPS']) ? "https://" : "http://") . $_SERVER["HTTP_HOST"] ?>/css/font-awesome.min.css" rel="stylesheet" type="text/css">
